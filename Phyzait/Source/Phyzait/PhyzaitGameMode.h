f520c7a3f107aa1b4b226cac028eafa48d58bbb7
Bobo <1761628865@qq.com>
2025-01-16 14:14:08 +0800

    添加UE第三人称C++项目

---
+++ Phyzait/Source/Phyzait/PhyzaitGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhyzaitGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APhyzaitGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APhyzaitGameMode();
+};
+
+
+
